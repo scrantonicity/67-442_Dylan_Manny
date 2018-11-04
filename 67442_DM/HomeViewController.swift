8e6f5e1dc8bf0721a985243dc9e844c5b3f1a331
Dylan Hyun <dchyun@andrew.cmu.edu>
2018-11-04 15:31:16 -0500

    added welcome screen and storyboard

---
+++ 67442_DM/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  67442_DM
+//
+//  Created by Dylan Hyun on 11/4/18.
+//  Copyright © 2018 Dylan Hyun. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
