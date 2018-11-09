900d06becc90bc36ddbcbe1c89f14d5587c68de4
Dylan Hyun <dchyun@andrew.cmu.edu>
2018-11-09 13:31:44 -0500

    added articles tests

---
+++ 67442_DMTests/ArticlesTests/ArticleDetailsViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  ArticlesDetailViewModelTests.swift
+//  67442_DMTests
+//
+//  Created by Dylan Hyun on 11/9/18.
+//  Copyright © 2018 Dylan Hyun. All rights reserved.
+//
+
+import XCTest
+@testable import _7442_DM
+
+class ArticleDetailsViewModelTests: XCTestCase {
+  
+  
+
+}
