14cd28b3bee0618e1d9b918136e0f5109c33c834
Dylan Hyun <dchyun@andrew.cmu.edu>
2018-11-09 11:44:53 -0500

    SongsModel tests

---
+++ 67442_DMTests/_7442_DMTests.swift
@@ -0,0 +1,33 @@
+//
+//  _7442_DMTests.swift
+//  67442_DMTests
+//
+//  Created by Dylan Hyun on 11/9/18.
+//  Copyright © 2018 Dylan Hyun. All rights reserved.
+//
+
+import XCTest
+
+class _7442_DMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
