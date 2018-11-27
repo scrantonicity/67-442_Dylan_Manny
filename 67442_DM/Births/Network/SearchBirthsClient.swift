9b3619201350840ef69c27fca75cc280a4302bf6
Manuel Lopez <mflopez@andrew.cmu.edu>
2018-11-26 21:23:32 -0500

    created rough draft of birth models and views. Moved articles models out of articles

---
+++ 67442_DM/Births/Network/SearchBirthsClient.swift
@@ -0,0 +1,28 @@
+//
+//  SearchBirthsClient.swift
+//  67442_DM
+//
+//  Created by Manuel Lopez on 11/26/18.
+//  Copyright © 2018 Dylan Hyun. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class SearchWikiClient {
+  func fetchWikiInfo(month: Int, day: Int, _ completion: @escaping (Data?) -> Void) {
+    
+    //Must be https or else this will complain
+    let urlString = "https://history.muffinlabs.com/date/\(month)/\(day)"
+    
+    Alamofire.request(urlString).response { response in
+      if let error = response.error {
+        print("Error fetching repositories: \(error)")
+        completion(response.data)
+        return
+      }
+      completion(response.data)
+    }
+    
+  }
+}
