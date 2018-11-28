255c146285e90ee75600b9f26e5eb7997b3eae9a
Manuel Lopez <mflopez@andrew.cmu.edu>
2018-11-27 20:27:29 -0500

    started creating death view, files basically mirror births, haven't hooked up to interface yet.

---
+++ 67442_DM/Deaths/Network/SearchDeathsClient.swift
@@ -0,0 +1,12 @@
+//
+//  SearchDeathsClient.swift
+//  67442_DM
+//
+//  Created by Manuel Lopez on 11/27/18.
+//  Copyright © 2018 Dylan Hyun. All rights reserved.
+//
+
+import Foundation
+
+//Calls from SearchBirthClient File
+// uses SearchWikiClient Class
