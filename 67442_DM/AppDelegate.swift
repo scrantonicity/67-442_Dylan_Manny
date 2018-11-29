967b68ba1c53f413eb6defcbbbb979776858f28b
Dylan Hyun <dchyun@andrew.cmu.edu>
2018-11-29 16:12:56 -0500

    no spotify auth at start

--- 67442_DM/AppDelegate.swift
+++ 67442_DM/AppDelegate.swift
@@ -59,7 +59,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func applicationDidBecomeActive(_ application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    spotifyManager.authorize()
+//    spotifyManager.authorize()
   }
   
   func applicationWillTerminate(_ application: UIApplication) {
